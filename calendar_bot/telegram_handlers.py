02b38a1cf6ae02203adffe33814b168d2f2ba7d2
OlegVeshy <olegveshy@MacBook-Air-OlegVeshy.local>
2026-04-28 17:19:24 +0300

    Added /list telegram message handler

--- calendar_bot/telegram_handlers.py
+++ calendar_bot/telegram_handlers.py
@@ -1,9 +1,9 @@
 from aiogram import Dispatcher
-from aiogram.filters import CommandStart
+from aiogram.filters import CommandStart, Command
 from aiogram.types import Message
 
 from calendar_bot.llm_parser import parse_event
-from calendar_bot.config import Config, load_config
+from calendar_bot.config import Config
 from calendar_bot.events import Event
 from calendar_bot import sql_storage
 
@@ -11,6 +11,22 @@ def register_telegram_handlers(dp: Dispatcher, config: Config) -> None:
     @dp.message(CommandStart())
     async def start_handler(message: Message) -> None:
         await message.answer("Привет! Я CalendarBot.")
+
+    @dp.message(Command("list"))
+    async def list_handler(message: Message) -> None:
+        if message.from_user is None:
+            await message.answer("Возникла ошибка при обработке автора сообщения.")
+            return 
+        
+        user_id = message.from_user.id
+        events_list = sql_storage.list_events(config.database_path, user_id)
+
+        answer = "==== Текущие события ====\n"
+        for event in events_list:
+            answer += event.__repr__()
+        
+        await message.answer(answer)
+
     
     @dp.message()
     async def text_handler(message: Message) -> None:
@@ -28,7 +44,7 @@ def register_telegram_handlers(dp: Dispatcher, config: Config) -> None:
         parsed_event = parse_event(text, config.openai_api_key)
 
         event = Event.from_parsed(user_id, parsed_event)
-        database_path = load_config().database_path
+        database_path = config.database_path
 
         sql_storage.add_event(database_path, event)
         await message.answer("Cобытие успешно добавлено")
